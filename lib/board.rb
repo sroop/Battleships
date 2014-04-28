7895f6cb87069c2d3eedcb5c539abf593cfc3516
Sroop <sroopsunar@hotmail.com>
2014-04-28 15:43:06 +0100

    board refactored

--- lib/board.rb
+++ lib/board.rb
@@ -1,8 +1,11 @@
 require 'ship'
 require 'water'
 require 'cell'
+require_relative './shipbuilder'
 
 class Board
+
+	include ShipBuilder
 
 	attr_reader :player, :grid
 
@@ -26,88 +29,28 @@ class Board
 	end
 
   	def place_aircraft_carrier
-  		@grid[random_coordinate_generator] = Cell.new(Ship.new)
-  		@grid[second_coordinate] = Cell.new(Ship.new)
-  		@grid[third_coordinate] = Cell.new(Ship.new)
-  		@grid[fourth_coordinate] = Cell.new(Ship.new)
-  		@grid[fifth_coordinate] = Cell.new(Ship.new)
+  		ship_length_five
 	end
 
   	def place_battleship
-  		@grid[random_coordinate_generator] = Cell.new(Ship.new)
-  		@grid[second_coordinate] = Cell.new(Ship.new)
-  		@grid[third_coordinate] = Cell.new(Ship.new)
-  		@grid[fourth_coordinate] = Cell.new(Ship.new)
+  		ship_length_four
 	end
 
 	def place_cruiser
-  		@grid[random_coordinate_generator] = Cell.new(Ship.new)
-  		@grid[second_coordinate] = Cell.new(Ship.new)
-  		@grid[third_coordinate] = Cell.new(Ship.new)
+		ship_length_three
 	end
 
 	def place_submarine
-		place_cruiser
+		ship_length_three
 	end
 
 	def place_destroyer
-		place_cruiser
+		ship_length_three
 	end
 
 	def place_patrol_boat
-  		@grid[random_coordinate_generator] = Cell.new(Ship.new)
-  		@grid[second_coordinate] = Cell.new(Ship.new)
+		ship_length_two
 	end
-
-
-	def register_shot(coordinates)
-		@grid[coordinates].content.hit!
-	end
-	
-	def random_coordinate_generator
-		letter = ("A".."J").to_a.sample
-		number = (1..10).to_a.sample.to_s
-		random_coordinate = letter + number
-		return random_coordinate
-	end
-
-	def second_coordinate
-		first_spot = random_coordinate_generator.chars
-		first_spot[1] = first_spot[1].next
-		ship_two = first_spot.join
-		return ship_two
-	end
-
-	def third_coordinate
-		second_spot = second_coordinate.chars
-		second_spot[1] = second_spot[1].next
-		ship_three = second_spot.join
-		return ship_three
-	end
-
-	def fourth_coordinate
-		third_spot = third_coordinate.chars
-		third_spot[1] = third_spot[1].next
-		ship_four = third_spot.join
-		return ship_four
-	end
-
-	def fifth_coordinate
-		fourth_spot = fourth_coordinate.chars
-		fourth_spot[1] = fourth_spot[1].next
-		ship_five = fourth_spot.join
-		return ship_five
-	end
-
-
-
-	# def next_number_along(at_coordinates)
-	# 	split = at_coordinates.chars
-	# 	split[1] = split[1].next
-	# 	split.join
-	# 	return split
-	# end
-
 
 end
 
---
+++ lib/shipbuilder.rb
@@ -0,0 +1,68 @@
+module ShipBuilder
+
+	def register_shot(coordinates)
+		@grid[coordinates].content.hit!
+	end
+	
+	def random_coordinate_generator
+		letter = ("A".."J").to_a.sample
+		number = (1..10).to_a.sample.to_s
+		random_coordinate = letter + number
+		return random_coordinate
+	end
+
+	def second_coordinate
+		first_spot = random_coordinate_generator.chars
+		first_spot[1] = first_spot[1].next
+		ship_two = first_spot.join
+		return ship_two
+	end
+
+	def third_coordinate
+		second_spot = second_coordinate.chars
+		second_spot[1] = second_spot[1].next
+		ship_three = second_spot.join
+		return ship_three
+	end
+
+	def fourth_coordinate
+		third_spot = third_coordinate.chars
+		third_spot[1] = third_spot[1].next
+		ship_four = third_spot.join
+		return ship_four
+	end
+
+	def fifth_coordinate
+		fourth_spot = fourth_coordinate.chars
+		fourth_spot[1] = fourth_spot[1].next
+		ship_five = fourth_spot.join
+		return ship_five
+	end
+
+		def ship_length_two
+		@grid[random_coordinate_generator] = Cell.new(Ship.new)
+  		@grid[second_coordinate] = Cell.new(Ship.new)
+	end
+
+	def ship_length_three
+		@grid[random_coordinate_generator] = Cell.new(Ship.new)
+  		@grid[second_coordinate] = Cell.new(Ship.new)
+  		@grid[third_coordinate] = Cell.new(Ship.new)
+	end
+
+	def ship_length_four
+		@grid[random_coordinate_generator] = Cell.new(Ship.new)
+  		@grid[second_coordinate] = Cell.new(Ship.new)
+  		@grid[third_coordinate] = Cell.new(Ship.new)
+  		@grid[fourth_coordinate] = Cell.new(Ship.new)
+	end
+
+	def ship_length_five
+		@grid[random_coordinate_generator] = Cell.new(Ship.new)
+  		@grid[second_coordinate] = Cell.new(Ship.new)
+  		@grid[third_coordinate] = Cell.new(Ship.new)
+  		@grid[fourth_coordinate] = Cell.new(Ship.new)
+  		@grid[fifth_coordinate] = Cell.new(Ship.new)
+	end
+
+end
