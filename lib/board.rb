ca8c727e07c29151979e6f0390f0c6d27e3f3323
Robert Leon <llexi@hotmail.com>
2014-04-28 15:29:48 +0100

    Created methods for placement of each length of ship

--- lib/board.rb
+++ lib/board.rb
@@ -27,11 +27,38 @@ class Board
 
   	def place_aircraft_carrier
   		@grid[random_coordinate_generator] = Cell.new(Ship.new)
-  		@grid[ship_length_2] = Cell.new(Ship.new)
-  		@grid[ship_length_3] = Cell.new(Ship.new)
-  		@grid[ship_length_4] = Cell.new(Ship.new)
-  		@grid[ship_length_5] = Cell.new(Ship.new)
+  		@grid[second_coordinate] = Cell.new(Ship.new)
+  		@grid[third_coordinate] = Cell.new(Ship.new)
+  		@grid[fourth_coordinate] = Cell.new(Ship.new)
+  		@grid[fifth_coordinate] = Cell.new(Ship.new)
 	end
+
+  	def place_battleship
+  		@grid[random_coordinate_generator] = Cell.new(Ship.new)
+  		@grid[second_coordinate] = Cell.new(Ship.new)
+  		@grid[third_coordinate] = Cell.new(Ship.new)
+  		@grid[fourth_coordinate] = Cell.new(Ship.new)
+	end
+
+	def place_cruiser
+  		@grid[random_coordinate_generator] = Cell.new(Ship.new)
+  		@grid[second_coordinate] = Cell.new(Ship.new)
+  		@grid[third_coordinate] = Cell.new(Ship.new)
+	end
+
+	def place_submarine
+		place_cruiser
+	end
+
+	def place_destroyer
+		place_cruiser
+	end
+
+	def place_patrol_boat
+  		@grid[random_coordinate_generator] = Cell.new(Ship.new)
+  		@grid[second_coordinate] = Cell.new(Ship.new)
+	end
+
 
 	def register_shot(coordinates)
 		@grid[coordinates].content.hit!
@@ -44,29 +71,29 @@ class Board
 		return random_coordinate
 	end
 
-	def ship_length_2
+	def second_coordinate
 		first_spot = random_coordinate_generator.chars
 		first_spot[1] = first_spot[1].next
 		ship_two = first_spot.join
 		return ship_two
 	end
 
-	def ship_length_3
-		second_spot = ship_length_2.chars
+	def third_coordinate
+		second_spot = second_coordinate.chars
 		second_spot[1] = second_spot[1].next
 		ship_three = second_spot.join
 		return ship_three
 	end
 
-	def ship_length_4
-		third_spot = ship_length_3.chars
+	def fourth_coordinate
+		third_spot = third_coordinate.chars
 		third_spot[1] = third_spot[1].next
 		ship_four = third_spot.join
 		return ship_four
 	end
 
-	def ship_length_5
-		fourth_spot = ship_length_4.chars
+	def fifth_coordinate
+		fourth_spot = fourth_coordinate.chars
 		fourth_spot[1] = fourth_spot[1].next
 		ship_five = fourth_spot.join
 		return ship_five
